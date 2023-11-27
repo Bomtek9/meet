92a3a02bcf9c2658b3454344a74878d8cd67bd25
Bomtek9 <clayduplantis9@gmail.com>
2023-11-27 12:11:17 -0600

    Render list of events test

--- src/_tests_/App.test.js
+++ src/_tests_/App.test.js
@@ -3,4 +3,7 @@
 import { render } from "@testing-library/react";
 import App from "../App";
 
-describe("<App /> component", () => {});
+describe(test("renders list of events"), () => {
+  const AppDOM = render(<App />).container.firstChild;
+  expect(AppDOM.querySelector("#event-list")).toBeInTheDocument();
+});
