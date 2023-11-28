958096d9b9dff293ae99e1f4ab46dbd74eb05bb8
Bomtek9 <clayduplantis9@gmail.com>
2023-11-28 15:45:46 -0600

    added api data into code

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,7 +1,23 @@
-// src/components/Event.js
+import { useState } from "react";
 
-const Event = () => {
-  return <li></li>;
+const Event = ({ event }) => {
+  const [showDetails, setShowDetails] = useState(false);
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+  return (
+    <li>
+      <div className="event">
+        <h2 style={{ color: "#1e847f" }}>{event.summary}</h2>
+        <div className="location">{event.location} </div>
+        <div className="dateTime">{event.start.dateTime}</div>
+        {showDetails && <div className="description">{event.description}</div>}
+        <button className="details-btn" onClick={toggleDetails}>
+          {showDetails ? "Hide Details" : "Show Details"}
+        </button>
+      </div>
+    </li>
+  );
 };
 
 export default Event;
