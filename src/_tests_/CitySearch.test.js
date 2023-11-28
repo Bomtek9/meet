ec521ff27ad71e5a4386bbb8f200ca3f27879e4f
Bomtek9 <clayduplantis9@gmail.com>
2023-11-28 12:51:17 -0600

    Created Mock Data
    api data from google events

--- src/_tests_/CitySearch.test.js
+++ src/_tests_/CitySearch.test.js
@@ -5,6 +5,21 @@ import CitySearch from "../components/CitySearch";
 import userEvent from "@testing-library/user-event";
 
 describe("<CitySearch /> component", () => {
+  test("suggestion list is hidden by default", () => {
+    const CitySearchComponent = render(<CitySearch />);
+    const suggestionList = CitySearchComponent.queryByRole("list");
+    expect(suggestionList).not.toBeInTheDocument();
+  });
+
+  test("renders a list of suggestions when city textbox gains focus", async () => {
+    const CitySearchComponent = render(<CitySearch />);
+    const cityTextBox = CitySearchComponent.queryByRole("textbox");
+    await userEvent.click(cityTextBox); // Use userEvent.click directly
+    const suggestionList = CitySearchComponent.queryByRole("list");
+    expect(suggestionList).toBeInTheDocument();
+    expect(suggestionList).toHaveClass("suggestions");
+  });
+
   test("renders text input", () => {
     const CitySearchComponent = render(<CitySearch />);
     const cityTextBox = CitySearchComponent.queryByRole("textbox");
