1f5cd30478b15ed918e52fe1f8e50a1ad64bcfd6
Bomtek9 <clayduplantis9@gmail.com>
2023-12-18 13:47:29 -0600

    Update CityEventsChart.js

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -11,12 +11,12 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-const CityEventsChart = (allLocations, events) => {
+const CityEventsChart = ({ allLocations, events }) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     setData(getData());
-  }, [`${events}`]);
+  }, [`${data}`]);
 
   const getData = () => {
     const data = allLocations.map((location) => {
@@ -28,25 +28,25 @@ const CityEventsChart = (allLocations, events) => {
     });
     return data;
   };
-};
 
-return (
-  <ResponsiveContainer width="99%" height={400}>
-    <ScatterChart
-      margin={{
-        top: 20,
-        right: 20,
-        bottom: 20,
-        left: 20,
-      }}
-    >
-      <CartesianGrid />
-      <XAxis type="number" dataKey="x" name="stature" unit="cm" />
-      <YAxis type="number" dataKey="y" name="weight" unit="kg" />
-      <Tooltip cursor={{ strokeDasharray: "3 3" }} />
-      <Scatter name="A school" data={data} fill="#8884d8" />
-    </ScatterChart>
-  </ResponsiveContainer>
-);
+  return (
+    <ResponsiveContainer width="99%" height={400}>
+      <ScatterChart
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20,
+        }}
+      >
+        <CartesianGrid />
+        <XAxis type="number" dataKey="x" name="stature" unit="cm" />
+        <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+        <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+        <Scatter name="A school" data={data} fill="#8884d8" />
+      </ScatterChart>
+    </ResponsiveContainer>
+  );
+};
 
 export default CityEventsChart;
