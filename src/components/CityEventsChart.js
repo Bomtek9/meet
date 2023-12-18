8ec0a030259e51d740f2e2be9578834da17590fc
Bomtek9 <clayduplantis9@gmail.com>
2023-12-18 15:00:10 -0600

    Update CityEventsChart.js
    adjusted margin

--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -35,8 +35,8 @@ const CityEventsChart = ({ allLocations, events }) => {
         margin={{
           top: 20,
           right: 20,
-          bottom: 20,
-          left: 20,
+          bottom: 60,
+          left: -30,
         }}
       >
         <CartesianGrid />
